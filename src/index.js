f7e86f6647efd55000a8198245a678fccc1b9bd4
Lauren Perchitti <laurenperchitti@gmail.com>
2018-08-02 18:00:47 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ToggleButton from './components/ToggleButton';
+import Address from './components/Address'
+
 
 ReactDOM.render(
   <ToggleButton />,
+  <Address />,
   document.getElementById('root')
 )
